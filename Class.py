628157310d1bafb87374024b9a8add4c7d4e4f64
Drew Tuckey <aptuckey@millersville.edu>
2021-02-04 16:48:00 -0500

    Updated ClassCollection and added Class.py

---
+++ Class.py
@@ -0,0 +1,19 @@
+### File: Class.py
+### Classes defined: Class
+
+class Class():
+
+        def __init__(self, name):
+            self.name = name
+            self.attributeDict = {}
+            
+        def rename(self, oldName, newName):
+        
+        def addAttribute(self, name):
+            
+
+        def deleteAttribute(self, name):
+            
+       
+        def renameAttribute(self, oldName, newName):
+           
--- ClassCollection.py
+++ ClassCollection.py
@@ -1,17 +1,22 @@
 ### File: ClassCollection.py
 ### Classes defined: ClassCollection
 
+import Class
+
 class ClassCollection():
 
         ## Initializes a ClassCollection Object 
         ## with a dictionary that will be filled
         ## with Classes
         ##
-        ##Ex:   collection1 = ClassCollection()
+        ## Ex:   collection1 = ClassCollection()
         def __init__(self):         
-            self.classDict = []
+            self.classDict = {}
+            self.relationshipDict = {}
 
-       
+        # self.getClass(name).addAttribute(attributeName)
+        # self.addAttribute(className, attributeName) -> className.addAttribute(attributeName)
+
         ## Cast the user's name input to a string, creates
         ## a new dictionary entry for that name and assigns
         ## it the value of the class Object
@@ -25,7 +30,7 @@ class ClassCollection():
         ## that entry. Remove existing relationsips that contain 
         ## that class name.
         ##
-        ##Ex:   collection1.deleteClass("ClassX")
+        ## Ex:   collection1.deleteClass("ClassX")
         def deleteClass(self, name):
             
         ## Cast the user's name input to a string, search 
@@ -36,6 +41,22 @@ class ClassCollection():
         ## class. Pending on how the relationships are 
         ## implemented.
         ##
-        ##Ex:   collection1.renameClass("ClassX", "ClassY")
-        def renameClass(self, oldName, newName):
+        ## Ex:   collection1.renameClass("ClassX", "ClassY")
+        def renameClass(self, oldName, newName):        
+
+
+
+        def addRelationship(self, firstClassName, secondClassName):
+        
+        def deleteRelationship(self, firstClassName, secondClassName):
+            
+
            
+        def addAttribute(self, className, attributeName):
+        
+        def deleteAttribute(self, className, attributeName):
+            
+        def renameAttribute(self, className, oldAttributeName, newAttributeName):
+
+
+
