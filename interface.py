85f379a0679b32680238e6e75d37f67abab5587b
Joe Malone <jemalone@millersville.edu>
2021-02-08 15:35:50 -0500

    Added ListClass functionality to the interface.

---
+++ interface.py
@@ -0,0 +1,13 @@
+### List class
+import json
+### Interface.py
+
+def listClass(collection, name):
+    # check if class exists in the collection
+    # if it doesn't, error
+    if name not in collection.classDict:
+        raise KeyError(f"Class {name} not found")
+
+    print(f"-{name}")
+    for attr in collection.classDict[name].attributeDict:
+        print(f"\t* {attr}")
