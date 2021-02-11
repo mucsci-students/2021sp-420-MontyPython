dabaff20ed0c41fe6041aa72965a61894bc88d47
Jen <74936440+Jen04@users.noreply.github.com>
2021-02-10 20:57:53 -0500

    Added list classes and exit, created REPL
    The interface file is a bit odd because there are a few versions on various branches, so I took some of the stuff from the current interface branch. I removed safe file/load file because those haven't been added through a pull request yet

--- Interface.py
+++ Interface.py
@@ -4,13 +4,18 @@ import sys
 
 ### List classes
 def listClasses(collection):
-    #todo
-    pass
+    for currentClass in collection.classDict:
+        listClass(collection, currentClass)
 
 ### List class
 def listClass(collection, name):
-    #todo
-    pass
+    # check if class exists in the collection
+    if name not in collection.classDict:
+        raise KeyError(f"Class {name} not found")
+
+    print(f"-{name}")
+    for attr in collection.classDict[name].attributeDict:
+        print(f"\t* {attr}")
 
 ### List relationships
 def listRelationships(collection):
@@ -24,47 +29,12 @@ def listRelationships(collection):
         #       outputClassDict[name] = attr
         # relationships list = classcollection.relationshipDict
 def saveFile(collection, fileName):
-    nameList = []
-    attributesList = []
-    for classObj in collection.classDict.values():
-        nameList.append(classObj.name)
-        attributesList.append(classObj.attributeDict)
-
-    classesDictionary = {}
-    for name, attributes in zip(nameList, attributesList):
-        classesDictionary[name] = attributes
-
-    relationshipsDictionary = {}
-    for key, value in collection.relationshipDict.items():
-        stringKey = ', '.join(str(s) for s in key)
-        relationshipsDictionary[stringKey] = value
-
-    classesRelationshipsList = [classesDictionary, relationshipsDictionary]
-
-    fileName += ".monty"
-    with open(fileName, "w") as f:
-        json.dump(classesRelationshipsList, f)
+    pass
     
 
 ### Load
 def loadFile(collection, fileName):
-    collection.classDict = {}
-    collection.relationshipDict = {}
-    classesRelationshipsList = []
-    fileName += ".monty"
-    with open(fileName, "r") as f:
-        classesRelationshipsList = json.load(f)
-
-    classesDictionary = classesRelationshipsList[0]
-    relationshipsDictionary = classesRelationshipsList[1]
-
-    for key, value in relationshipsDictionary.items():
-        tupleKey = tuple(key.split(", "))
-        collection.relationshipDict[tupleKey] = value
-
-    for name, attributes in classesDictionary.items():
-        collection.addClass(name)
-        collection[name].attributeDict = attributes
+    pass
         
         
 ### Help
