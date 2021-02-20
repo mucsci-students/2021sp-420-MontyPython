a1e87b297a2ab26d9ab6fc7173a93ad2ad6fb8cf
Drew Tuckey <aptuckey@millersville.edu>
2021-02-20 00:35:48 -0500

    added Feild class and made modifications to ClassCollection and Class

---
+++ Field.py
@@ -0,0 +1,8 @@
+### File: Field.py
+### Classes defined: Field
+class Field:
+    def __init__(self, name, dataType):
+        self.name = name
+        self.dataType = dataType
+    
+   
---
+++ FieldTest.py
@@ -0,0 +1,75 @@
+import unittest
+from ClassCollection import ClassCollection
+from Class import Class
+from Field import Field
+
+
+class FieldTests(unittest.TestCase):
+
+    # Checks if field is in the fieldDict
+    def testAddField(self):
+        collection = ClassCollection() 
+        collection.addClass("A")
+        collection.addField("A", "name", "String")
+        self.assertIsNotNone( collection.getField("A", "name"))
+
+    # Checks if a field is successfully deleted
+    def testDeleteField(self):
+        collection = ClassCollection() 
+        collection.addClass("A")
+        collection.addField("A", "name", "String")
+        collection.deleteField("A", "name")
+        self.assertIsNone( collection.getField("A", "name"))
+
+
+     # Checks if a class is successfully deleted
+    def testRenameField(self):
+        collection = ClassCollection() 
+        collection.addClass("A")
+        collection.addField("A", "name", "String")
+        collection.renameField("A", "name", "surname")
+        self.assertIsNotNone( collection.getField("A", "surname"))
+
+    #rename Class with attributes. Ensure there is no leftover Class
+    def testRenameFieldComplex(self):
+        collection = ClassCollection() 
+        collection.addClass("A")
+        collection.addField("A", "name", "String")
+        collection.renameField("A", "name", "surname")
+        self.assertIsNone( collection.getField("A", "name"))
+        self.assertRaises(KeyError, collection.addField("A", "surname", "String"))
+        collection.deleteField("A", "surname")
+        self.assertIsNone( collection.getField("A", "name"))
+        self.assertIsNone( collection.getField("A", "surname"))
+
+    #test error on adding duplicate classes
+    def testAddDuplicateField(self):
+        collection = ClassCollection()
+        collection.addClass("A")
+        collection.addField("A", "name", "String")
+        self.assertRaises(KeyError, collection.addField("A", "name", "String"))
+    
+    #test error on removing nonexisted classes
+    def testRemoveNonExistentField(self):
+        collection = ClassCollection() 
+        collection.addClass("A")
+        self.assertRaises(KeyError, collection.deleteField("A", "name"))
+
+    #test error on renaming duplicate classes
+    def testRenameDuplicateField(self):
+        collection = ClassCollection() 
+        collection.addClass("A")
+        collection.addField("A", "name", "String")
+        collection.addField("A", "surname", "String")
+        self.assertRaises(KeyError,  collection.renameField("A", "name", "surname"))
+
+    #test error on removing nonexisted classes
+    def testRenameNonExistentField(self):
+        collection = ClassCollection() 
+        collection.addClass("A")
+        collection.addField("A", "name", "String")
+        collection.addField("A", "surname", "String")
+        self.assertRaises(KeyError,  collection.renameField("A", "joename", "surname"))
+   
+if __name__ == '__main__':
+    unittest.main()
