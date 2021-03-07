37b7a61fda7892d74b2aff6389053c849362897d
Sean Malloy <sfmalloy@millersville.edu>
2021-03-07 11:34:06 -0500

    Take 2 with virtualenv port

--- virtualenv/Scripts/activate.bat
+++ virtualenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\CSCI420\2021sp-420-MontyPython
+set VIRTUAL_ENV=D:\CSCI420\2021sp-420-MontyPython\virtualenv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(2021sp-420-MontyPython) %PROMPT%
+set PROMPT=(virtualenv) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
