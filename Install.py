c58306aa6ae520f6a57769f88772bef2704255b1
Sean Malloy <sfmalloy@millersville.edu>
2021-03-07 13:25:39 -0500

    Edited README

--- Install.py
+++ Install.py
@@ -1,5 +1,8 @@
 import subprocess
 
-cmd = 'python -m pip install --disable-pip-version-check -r requirements.txt'
-subprocess.run(cmd.split())
-print('Installation finished!')
+try:
+    cmd = 'python -m pip install --disable-pip-version-check -r requirements.txt'
+    subprocess.run(cmd.split())
+    print('Installation finished!')
+except:
+    print('Installation failed, please follow the README for setting up a virtual environment')
