bec6b3cb58d25f497ddd2a6dcd1749000b408d71
Jen <74936440+Jen04@users.noreply.github.com>
2021-03-25 13:31:55 -0400

    Fixing some line stuff so the program runs

--- GUIMainWindow.py
+++ GUIMainWindow.py
@@ -10,12 +10,13 @@ class MainWindow(Frame):
         # Master is root   
         self.master = master
 
-        # Sets up window layout
-        self.setup()      
-  
         self.lineDict = []
         self.lineObjList = []
         self.classDict = {}
+
+        # Sets up window layout
+        self.setup()      
+
                        
         #(firstname, secondname): (x1, y1, x2, y2, type )
         #Types: 0 - aggregation 1 - Composition 2 - inheritance 3- realization
@@ -30,7 +31,7 @@ class MainWindow(Frame):
         #self.pack(fill=BOTH, expand=1)
 
         # Menu is set up in a different file to increase readability
-        self.menu = GUIMenuBar.menu(self, self.master) 
+        #self.menu = GUIMenuBar.menu(self, self.master) 
 
         # Create canvas for objects to be drawn on
         self.canvas = Canvas(self.master)
@@ -62,20 +63,24 @@ class MainWindow(Frame):
 
     def drawLines(self):
 
-        #clear the list of canvas objects
-        for i in self.lineObjList:
-            self.canvas.delete(i)
-        
-        #update the list of canvas objects using the lineDictionary
-        for key, x in self.lineDict.items():
-            if(x[4] == 0): #0 - aggregation 
-                self.lineObjList.append(self.canvas.create_line(x[0], x[1], x[2], x[3], arrow=LAST))
-            if (x[4] == 1): #1 - Composition
-                self.lineObjList.append(self.canvas.create_line(x[0], x[1], x[2], x[3], arrow=LAST))
-            if (x[4] == 2): #2 - inheritance
-                 self.lineObjList.append(self.canvas.create_line(x[0], x[1], x[2], x[3], arrow=LAST))
-            if(x[4] == 3): #3 - realization
-                 self.lineObjList.append(self.canvas.create_line(x[0], x[1], x[2], x[3], dash=(5, 1), arrow=LAST))
+        # If it's not empty
+        if self.lineObjList:
+            #clear the list of canvas objects
+            for i in self.lineObjList:
+                self.canvas.delete(i)
+            
+        # If it's not empty
+        if self.lineDict:
+            #update the list of canvas objects using the lineDictionary
+            for line in self.lineDict:
+                if(x[4] == 0): #0 - aggregation 
+                    self.lineObjList.append(self.canvas.create_line(x[0], x[1], x[2], x[3], arrow=LAST))
+                if (x[4] == 1): #1 - Composition
+                    self.lineObjList.append(self.canvas.create_line(x[0], x[1], x[2], x[3], arrow=LAST))
+                if (x[4] == 2): #2 - inheritance
+                    self.lineObjList.append(self.canvas.create_line(x[0], x[1], x[2], x[3], arrow=LAST))
+                if(x[4] == 3): #3 - realization
+                    self.lineObjList.append(self.canvas.create_line(x[0], x[1], x[2], x[3], dash=(5, 1), arrow=LAST))
     
     def addLine(self, firstClassName, secondClassName, x1, y1, x2, y2):
          self.lineDict[(firstClassName, secondClassName)] = [x1, y1, x2, y2]
