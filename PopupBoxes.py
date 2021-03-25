81327ec48143f435e0a088ff6641fe50efcda9df
Sean Malloy <sfmalloy@millersville.edu>
2021-03-24 22:19:36 -0400

    Add method box

--- PopupBoxes.py
+++ PopupBoxes.py
@@ -38,10 +38,10 @@ class GenericBox:
         lbl.configure(font=self.font)
         return lbl
     
-    def addEntry(self, r, c, stick=W, px=4, py=4, ipx=50):
+    def addEntry(self, r, c, stick=W, px=4, py=4, ipx=50, txt=''):
         entry = Entry(self.frame)
         entry.grid(row=r, column=c, sticky=stick, padx=px, pady=py, ipadx=ipx)
-        entry.configure(font=self.font)
+        entry.configure(font=self.font, text=txt)
         return entry
 
 # This needs to be modified, but it works
@@ -135,6 +135,86 @@ class RenameFieldBox(GenericBox):
 class AddMethodBox(GenericBox):
     def __init__(self, msg, controller):
         super().__init__(msg, controller)
+
+        PARAM_LIMIT = 10
+
+        self.addLabel('Class Name', 0, 0)
+        self.addLabel('Return Type', 1, 0)
+        self.addLabel('Method Name', 2, 0)
+        self.addLabel('Parameter Count', 4, 0)
+
+        self.addLabel('Overloaded Methods', 3, 0)
+        self.overloadLabel = self.addLabel('', 3, 1)
+
+        className = self.addEntry(0, 1)
+        returnType = self.addEntry(1, 1)
+        methodName = self.addEntry(2, 1)
+
+        self.paramTypes = []
+        self.paramNames = []
+        self.paramLabel = self.addLabel('', 5, 0)
+        self.paramCount = self.addEntry(4, 1, ipx=0)
+        self.oldParamCount = ''
+
+        # Yes I am defining a function within a function otherwise the lambda in
+        # the self.top.bind call is horrendous
+        # 
+        # This function essentially updates the Overloaded Methods label based
+        # on what is in the methodName textbox. If a method that is in there
+        # happens to exist, it will list every instance of the method within
+        # that class, that way the user knows not to add duplicate methods.
+        def keyEvent(event):
+            sv = StringVar()
+            if (className.get() in controller.model.classDict and 
+                methodName.get() in controller.model.classDict[className.get()].methodDict):
+                sv.set('\n'.join(map(str, controller.model.classDict[className.get()].methodDict[methodName.get()])))
+            self.overloadLabel.configure(textvariable=sv, justify=LEFT)
+
+            # Get the parameter count as a string from the paramCount textbox
+            pc = self.paramCount.get()
+            psv = StringVar()
+            if pc != '' and pc != self.oldParamCount:
+                # Update the oldParamCount so things boxes update only when
+                # they need to
+                self.oldParamCount = pc
+
+                # If need to update, yoink literally everything and put fresh boxes
+                if len(self.paramTypes) > 0:
+                    for t,n in zip(self.paramTypes, self.paramNames):
+                        t.destroy()
+                        n.destroy()
+                self.paramTypes = []
+                self.paramNames = []
+                if int(pc) > PARAM_LIMIT:
+                    psv.set('Parameter count too high')
+                    self.paramLabel.configure(textvariable=psv, justify=LEFT)
+                    return
+                psv.set('Parameters (type followed by name)')
+                self.paramLabel.configure(textvariable=psv, justify=LEFT)
+                for r in range(6, 6+int(pc)):
+                    self.paramTypes.append(self.addEntry(r, 0, ipx=50))
+                    self.paramNames.append(self.addEntry(r, 1, ipx=50))
+            elif pc != self.oldParamCount:
+                self.oldParamCount = pc
+                if len(self.paramTypes) > 0:
+                    for t,n in zip(self.paramTypes, self.paramNames):
+                        t.destroy()
+                        n.destroy()
+                self.paramTypes = []
+                self.paramNames = []
+                psv.set('')
+                self.paramLabel.configure(textvariable=sv, justify=LEFT)
+
+        self.top.bind('<Key>', lambda e: keyEvent(e))
+
+        def buttonEvent(event):
+            params = []
+            for t,n in zip(self.paramTypes, self.paramNames):
+                params.append([t.get(), n.get()])
+            controller.addMethod(className.get(), methodName.get(), returnType.get(), params)
+        self.addButton('Add', 6+PARAM_LIMIT, 0, SW, buttonEvent)
+        self.addButton('Cancel', 6+PARAM_LIMIT, 1, SE, self.top.destroy)
+
 
 class DeleteMethodBox(GenericBox):
     def __init__(self, msg, controller):
