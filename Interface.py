085321978504b55213bdd39740fd7a45ce97bb2a
Quinn Lehman <qrlehman@millersville.edu>
2021-03-26 15:44:52 -0400

    Updates Save/Load to handle GUI, skeleton for coordinates

--- Interface.py
+++ Interface.py
@@ -24,11 +24,6 @@ def listRelationships(collection):
         print(f"\t{relationship[0]} -> {relationship[1]}")
 
 
-#{
-# (classA, classB) : relationship is currently ""
-# }
-#]
-
 #[
 #    {
 #    "classA" :
@@ -48,13 +43,37 @@ def listRelationships(collection):
 #    {
 #    (classA, classB) : type
 #    } 
+#    {
+#    "class A" : (X coord, Y coord)
+#    "class B" : (X coord, Y coord)
+#    }
 #]
 
 ### Save
-def saveFile(collection, fileName=None):
+
+#collection is a required parameter, referring to a ClassCollection object to save
+
+#fileName is a required parameter, referring to the file/path to save to.
+#   fileName's default value of None is used for failsafe error handling
+
+#GUI is an optional parameter, referring to save being invoked in the CLI vs GUI
+#   this defaults to saving in the CLI
+#   when saving in the GUI, mainWindow becomes required
+#   used for handling the dictionary of classes to their coordinate values
+#   set the string to "GUI" to save in GUI, this is case insensitive
+
+#coords is an optional parameter, referring to a coordinate dictionary in the GUI
+#   becomes required if GUI = "GUI" (case insensitive)
+#   currently unused, GUI coordinates aren't set for sure yet.
+
+def saveFile(collection, fileName=None, GUI="CLI", coords=None):
     #Value Error if no file name is given
-    if fileName == None:
+    if fileName == None or fileName.endswith("\\"):
         raise ValueError("No file name given to save")
+
+    #Adds file extension if not already included
+    if not fileName.endswith(".monty"):
+        fileName += ".monty"
 
     nameList = []
     methodsList = []
@@ -91,41 +110,83 @@ def saveFile(collection, fileName=None):
     for key, value in collection.relationshipDict.items():
         stringKey = ', '.join(str(s) for s in key)  
         relationshipsDictionary[stringKey] = value.typ
-
-    #The classes dictionary and the relationships dictionary are
-    #formed into a list, allowing for a singular structure to be dumped
-    classesRelationshipsList = [classesDictionary, relationshipsDictionary]
-
-    #Adds file extension
-    fileName += ".monty"
-    #Dumps classes and relationships list to file
-    with open(fileName, "w") as f:
-        json.dump(classesRelationshipsList, f)
     
+    coordsDictionary = {}
+    #If file already exists, pulls coordinates from loading the file
+    #Downside: not backwards compatible with old saved files
+    #Upside: easily preserves coordinates when editing in the CLI without
+    #having to have direct access or even have instances of the GUI classes open
+    priorExistence = os.path.exists(fileName)
+
+    #If file already exists and save is invoked in the CLI, loads file to find
+    #already present coordinates. Handles checks for added/deleted classes
+    #since the last save. Any class added in the CLI will have coords set to (-1, -1)
+    if priorExistence and GUI.lower() != "gui":
+        with open(fileName, "r") as f:
+            oldClassesRelationshipsCoordsList = json.load(f)
+            coordsDictionary = oldClassesRelationshipsCoordsList[2]
+
+        #Logic for updating class coordinates from existing file
+        #If class was removed, pops coords from coordsDictionary
+        #If class was added, defaults coords for that class to (-1, -1)
+        for key in coordsDictionary.keys():
+            if key not in classesDictionary.keys():
+                coordsDictionary.pop(key)
+        for key in classesDictionary.keys():
+            if key not in coordsDictionary.keys():
+                coordsDictionary[key] = (-1, -1)
+
+    #If file does not exist and save is invoked in the CLI,
+    #all class coords are set to the default of (-1, -1)
+    elif not priorExistence and GUI.lower() != "gui":
+        for key in classesDictionary.keys():
+            coordsDictionary[key] = (-1, -1)
+
+    #Regardless of file existence, if save is invoked in the GUI,
+    #uses the mainWindow object (which is required if saving in the GUI)
+    #to add/overwrite the coordinate list via the MainWindow object's classDict
+    else:
+        #temp defaulting until coord dictionary is decided upon
+        #different default value for testing
+        for key in classesDictionary.keys():
+            coordsDictionary[key] = (-2, -2)
+        #Uncomment, changed to proper formatting once GUI coordinates are ironed out
+        #coordsDictionary = coords
+
+    #classesDictionary, relationshipsDicttionary, and coordsDictionary are
+    #formed into a list to be json dumped to the supplied file
+    classesRelationshipsCoordsList = [classesDictionary, relationshipsDictionary, coordsDictionary]
+
+    with open(fileName, "w") as f:
+        json.dump(classesRelationshipsCoordsList, f)
 
 ### Load
 def loadFile(collection, fileName=None):
     #When no file name is supplied by the user, raises a Value Error
     if fileName == None:
         raise ValueError("No file name given to load")
-    #Adds file extension
-    fileName += ".monty"
+    
+    #Adds file extension if not already included
+    if not fileName.endswith(".monty"):
+        fileName += ".monty"
+
     #Check for file existence
     #Makes assumption that a file of extension .monty has the same
     #structure as created by the save function
     if os.path.isfile(fileName):
         collection.classDict = {}
         collection.relationshipDict = {}
-        classesRelationshipsList = []
+        classesRelationshipsCoordsList = []
         
         #File loading
         with open(fileName, "r") as f:
-            classesRelationshipsList = json.load(f)
+            classesRelationshipsCoordsList = json.load(f)
 
-        #Splits classes and relationships list into the component
-        #dictionaries
-        classesDictionary = classesRelationshipsList[0]
-        relationshipsDictionary = classesRelationshipsList[1]
+        #Splits classes,relationships, and coords dictionarys
+        #from the inputed list
+        classesDictionary = classesRelationshipsCoordsList[0]
+        relationshipsDictionary = classesRelationshipsCoordsList[1]
+        coordsDictionary = classesRelationshipsCoordsList[2]
 
         #Creates a class class for each entry in the classes dictionary
         #Within each class, creates an attribute class for each attribute
@@ -148,6 +209,10 @@ def loadFile(collection, fileName=None):
         for key, value in relationshipsDictionary.items():
             classes = key.split(", ")
             collection.addRelationship(classes[0], classes[1], value)
+
+        #Skeleton for coordinate dictionary when that gets settled in the GUI
+        for key, value in coordsDictionary.items():
+            pass
         
     #Raises an OS Error if the file does not exist
     else:
--- Tests/InterfaceTest.py
+++ Tests/InterfaceTest.py
@@ -21,7 +21,9 @@ class InterfaceTest(unittest.TestCase):
         with self.assertRaises(ValueError):
             Interface.saveFile(collection)       
 
-    #WARNING: deletes file1.monty in current directory to ensure file creation
+    #WARNING: deletes file1.monty in current directory to clean up after test
+    #If the test is failing, comment out the final line of "os.remove("file1.monty")"
+    #to manually check file contents
     def testCreatedSuccessfully(self):
         if os.path.isfile("file1.monty"):
             os.remove("file1.monty")
@@ -31,9 +33,34 @@ class InterfaceTest(unittest.TestCase):
         collection.addRelationship("foo", "bar", "aggregation")
         Interface.saveFile(collection, "file1")
         self.assertTrue(os.path.isfile("file1.monty"))
+        os.remove("file1.monty")
 
-    def testSaveCorrectFileContents(self):
-        comparisonString = "[{\"foo\": [{\"buzz\": [[\"int\", []], [\"int\", [[\"int\", \"a\"], [\"int\", \"b\"]]]]}, {\"test\": \"int\"}], \"bar\": [{}, {}]}, {\"foo, bar\": \"aggregation\"}]"
+    #WARNING: deletes file1.monty in current directory to clean up after test
+    #If the test is failing, comment out the final line of "os.remove("file1.monty")"
+    #to manually check file contents
+    #NOTE: test currently only checks for logic within save function
+    #Once GUI coordinates are ironed out, update test with non-dummy coordinates
+    #Will need to import GUI files to compare file's coordinates with actual ones
+    def testSaveCorrectFileContentsGUI(self):
+        comparisonString = "[{\"foo\": [{\"buzz\": [[\"int\", []], [\"int\", [[\"int\", \"a\"], [\"int\", \"b\"]]]]}, {\"test\": \"int\"}], \"bar\": [{}, {}]}, {\"foo, bar\": \"aggregation\"}, {\"foo\": [-2, -2], \"bar\": [-2, -2]}]"
+        collection = ClassCollection()
+        collection.addClass("foo")
+        collection.addClass("bar")
+        collection.addRelationship("foo", "bar", "aggregation")
+        collection.addMethod("foo", "buzz", "int", [])
+        collection.addMethod("foo", "buzz", "int", [("int", "a"), ("int", "b")])
+        collection.addField("foo", "test", "int")
+        Interface.saveFile(collection, "file1", "gui")
+        with open("file1.monty", "r") as f:
+            lines = f.readlines()
+            self.assertTrue((lines[0] == comparisonString) and (len(lines) == 1))
+        os.remove("file1.monty")
+
+    #WARNING: deletes file1.monty in current directory to clean up after test
+    #If the test is failing, comment out the final line of "os.remove("file1.monty")"
+    #to manually check file contents
+    def testSaveCorrectFileContentsCLI(self):
+        comparisonString = "[{\"foo\": [{\"buzz\": [[\"int\", []], [\"int\", [[\"int\", \"a\"], [\"int\", \"b\"]]]]}, {\"test\": \"int\"}], \"bar\": [{}, {}]}, {\"foo, bar\": \"aggregation\"}, {\"foo\": [-1, -1], \"bar\": [-1, -1]}]"
         collection = ClassCollection()
         collection.addClass("foo")
         collection.addClass("bar")
@@ -45,6 +72,7 @@ class InterfaceTest(unittest.TestCase):
         with open("file1.monty", "r") as f:
             lines = f.readlines()
             self.assertTrue((lines[0] == comparisonString) and (len(lines) == 1))
+        os.remove("file1.monty")
 
         
     def testLoadNoName(self):
@@ -60,6 +88,10 @@ class InterfaceTest(unittest.TestCase):
         with self.assertRaises(OSError):
             Interface.loadFile(collection, "file1")   
 
+    #WARNING: deletes file1.monty and file2.monty in current directory to
+    #clean up after test. If the test is failing, comment out the final lines of 
+    #"os.remove("file1.monty")" and "os.remove("file2.monty")" 
+    #to manually check file contents
     def testLoadCorrectFileContents(self):
         collection = ClassCollection()
         collection.addClass("foo")
@@ -72,6 +104,8 @@ class InterfaceTest(unittest.TestCase):
         Interface.loadFile(collectionLoaded, "file1")
         Interface.saveFile(collectionLoaded, "file2")
         self.assertTrue(filecmp.cmp("file1.monty", "file2.monty"))
+        os.remove("file1.monty")
+        os.remove("file2.monty")
 
     
 if __name__ == '__main__':
