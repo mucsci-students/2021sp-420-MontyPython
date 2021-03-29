6299bd6dd4f349bc50f82e90af85a416959929e4
Sean Malloy <sfmalloy@millersville.edu>
2021-03-28 23:57:06 -0400

    Fixed driver, added pattern

--- Monty.py
+++ Monty.py
@@ -1,12 +1,6 @@
 import argparse
 import sys
 
-try:
-    import PyQt5
-except ImportError:
-    print('Please run Install.py for required packages')
-    exit()
-
 parser = argparse.ArgumentParser('')
 parser.add_argument('--cli', action='store_true')
 args = parser.parse_args()
