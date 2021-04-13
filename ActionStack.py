06478cd3d8e4ea5fc6538f39aa7d9218bbd4ed92
Quinn Lehman <qrlehman@millersville.edu>
2021-04-13 16:41:31 -0400

    CLI Undo/Redo; tests, help file updates

---
+++ ActionStack.py
@@ -0,0 +1,31 @@
+class ActionStack():
+    def __init__(self, Momento):
+        #Entries in the stacks are Momento objects
+        self.undoStack = []
+        self.redoStack = []
+        self.currentObj = Momento
+        self.originalObj = Momento
+
+
+    #Called when an undoable command (excluding undo/redo)
+    #is called. Clears the redo stack to avoid illegal calls
+    def add(self, Momento):
+        self.undoStack.append(self.currentObj)
+        self.currentObj = Momento
+        self.redoStack = []
+
+    #Called when undo is called. Appends to redo stack
+    def undoPop(self):
+        if len(self.undoStack) != 0:
+            self.redoStack.append(self.currentObj)
+            self.currentObj = self.undoStack.pop()
+        else:
+            self.currentState = self.originalState
+
+    #Called when redo is called. Appends to undo stack
+    def redoPop(self):
+        if len(self.redoStack) != 0:
+            self.undoStack.append(self.currentObj)
+            self.currentObj = self.redoStack.pop()
+            self.undoStack.append(self.currentObj)
+        
--- REPL.py
+++ REPL.py
@@ -7,6 +7,7 @@ from colorama import init, Fore
 from ClassCollection import ClassCollection
 from Command import Command
 from Momento import Momento
+from ActionStack import ActionStack
 import Interface
 
 if os.name == 'nt':
@@ -18,8 +19,8 @@ class MontyREPL(cmd.Cmd):
     def __init__(self, completekey='tab', stdin=None, stdout=None):
         super().__init__(completekey, stdin, stdout)
         init(autoreset=True)
-        self.saveStates = []
         self.model = ClassCollection()
+        self.saveStates = ActionStack(Momento(Command("",""), self.model))
         self.intro = (Fore.GREEN + '\nMontyPython UML Editor\n' + '='*80 + 
             '\nType help [verbose|<command-name>] or ? for help on commands.\n' + Fore.RESET)
 
@@ -115,6 +116,14 @@ class MontyREPL(cmd.Cmd):
         else:
             self.help_load()
 
+    def do_undo(self, args):
+        self.saveStates.undoPop()
+        self.model = self.saveStates.currentObj.state
+
+    def do_redo(self, args):
+        self.saveStates.redoPop()
+        self.model = self.saveStates.currentObj.state
+
     # Classes
     def do_add_class(self, args):
         self.execute(self.model.addClass, args)
@@ -209,6 +218,10 @@ class MontyREPL(cmd.Cmd):
         self.print_cmd_help('save')
     def help_load(self):
         self.print_cmd_help('load')
+    def help_undo(self):
+        self.print_cmd_help('undo')
+    def help_redo(self):
+        self.print_cmd_help('redo')
 
     def help_add_class(self):
         self.print_cmd_help('add_class')
@@ -416,7 +429,14 @@ class MontyREPL(cmd.Cmd):
             args = args.split()
         command = Command(function, *args)
         command.execute()
-        self.saveStates.append(Momento(command, self.model))
+
+        #List of commands which cannot be undone or redone, as well as undo and redo
+        #Undo and redo are handled specially within their do functions
+        unusuals = ["help", "clear", "save", "load", "undo", "redo", "list_class", "list_classes", "list_relationships"]
+        if command.function not in unusuals:
+            self.saveStates.add(Momento(command, self.model))
+
+
     
     def onecmd(self, line):
         try:
