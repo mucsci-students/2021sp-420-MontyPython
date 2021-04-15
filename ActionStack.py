15ce34124033b9d80c290aab5448b927cd22596f
Quinn Lehman <qrlehman@millersville.edu>
2021-04-15 14:55:04 -0400

    bug fix - add after undo to original state

--- ActionStack.py
+++ ActionStack.py
@@ -12,7 +12,7 @@ class ActionStack():
     #Called when an undoable command (excluding undo/redo)
     #is called. Clears the redo stack to avoid illegal calls
     def add(self, Momento):
-        self.undoStack.append(self.currentObj)
+        self.undoStack.append(deepcopy(self.currentObj))
         self.currentObj = deepcopy(Momento)
         self.redoStack = []
 
@@ -22,15 +22,20 @@ class ActionStack():
             print("No actions to undo")
             self.currentObj = deepcopy(self.originalObj)
         else:   
-            self.redoStack.append(self.currentObj)
+            self.redoStack.append(deepcopy(self.currentObj))
             self.currentObj = deepcopy(self.undoStack.pop())
+
+            #Fringe case to fix being unable to remove a class added
+            #if added immediately after returning to the original state
+            if len(self.undoStack) == 0:
+                self.currentObj = deepcopy(self.originalObj)
 
     #Called when redo is called. Appends to undo stack
     def redoPop(self):
         if len(self.redoStack) == 0:
             print("No actions to redo")
         else:
-            self.undoStack.append(self.currentObj)
+            self.undoStack.append(deepcopy(self.currentObj))
             self.currentObj = deepcopy(self.redoStack.pop())
         
     def reset(self, Momento):
